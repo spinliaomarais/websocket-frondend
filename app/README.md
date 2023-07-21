c84125a8146304a462dae7ba97d3771ecbc6060e
spinliaomarais <spinliao@storemarais.com>
2023-07-21 18:05:50 +0800

    websocket fronend

---
+++ app/README.md
@@ -0,0 +1,25 @@
+# app
+
+websocket fronend example
+
+## Project Setup
+
+```sh
+nvm use v19
+```
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ app/vite.config.js
@@ -0,0 +1,32 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import path from 'path'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: path.resolve(__dirname, './src/assets/index.js'), // 1️⃣
+          dest: './', // 2️⃣
+        },
+        {
+          src: path.resolve(__dirname, './src/assets/main.css'), // 1️⃣
+          dest: './', // 2️⃣
+        },
+      ],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
+
